a88fc7698c5ff3de529f475596f475cbead54d5d
Zraz <zrazic@wp.pl>
2014-01-08 15:57:37 +0100

    keyCode into charCode

--- javascript.js
+++ javascript.js
@@ -46,7 +46,7 @@ function changePlayer() {
 
 window.onkeyup = function(event) {
   var keycode;
-  keycode = event.keyCode;
+  keycode = event.charCode;
   if (keycode === 32) { 
     changePlayer();
   }
