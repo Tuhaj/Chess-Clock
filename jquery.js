1b4d28451ebc74c79b467e8942f943ff68eb01aa
Zraz <zrazic@wp.pl>
2014-01-19 20:01:34 +0100

    refactoring, added folders, better tooltip button

--- jquery.js
+++ jquery.js
@@ -1,25 +1,29 @@
 //J QUERY
+var tooltip_is_on = true
 $(document).ready(function() {
   $("#info_button").click(function(){
     $("#info").toggleClass("hide_info");
     $("html, body").animate({ scrollTop: $(document).height() }, 2000);
-    blur();
+    $("#info_button").blur();
   });
 });
 
 $(document).ready(function() {
   $("#tips_button").click(function(){
+    if(tooltip_is_on) {
     $('.tooltip').tooltip('disable');
-    blur();
+    $("#tips_button").text("turn tooltips on");
+    tooltip_is_on = false
+    }
+    else {
+    $('.tooltip').tooltip('enable');
+    tooltip_is_on = true
+    $("#tips_button").text("turn tooltips off");
+    };
+    $("#tips_button").blur();
   });
 });
 
 $(function() {
     $( ".tooltip").tooltip();
   });
-
-$('#dropdown-1').on('show', function(event, dropdownData) {
-    console.log(dropdownData);
-}).on('hide', function(event, dropdownData) {
-    console.log(dropdownData);
-});
